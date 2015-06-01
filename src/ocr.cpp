110fc10ca31a7e961a86ea8c65261d85bf7c0475
Calvin Loncaric <loncaric@cs.washington.edu>
2015-06-01 15:52:39 -0700

    OCR improvements
    I think I have to drop this part of the project. :/

--- src/ocr.cpp
+++ src/ocr.cpp
@@ -1,14 +1,32 @@
 #include "ocr.hpp"
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 using namespace std;
 
+void dilate(Mat& img, int size) {
+    // see http://docs.opencv.org/doc/tutorials/imgproc/erosion_dilatation/erosion_dilatation.html
+    Mat element = getStructuringElement(
+        MORPH_RECT,
+        Size(2*size + 1, 2*size+1),
+        Point(size, size));
+    erode(img, img, element);
+}
+
 vector<TextBox> findText(const Mat& img) {
+    const double UPSCALE = 3.0;
+
+    Mat pp; // preprocessed
+    resize(img, pp, Size(0, 0), UPSCALE, UPSCALE, INTER_CUBIC);
+    dilate(pp, 1);
+    threshold(pp, pp, 230, 255, CV_THRESH_BINARY);
+
     tesseract::TessBaseAPI ocr;
 
     if (ocr.Init(NULL, "eng" /*, tesseract::OEM_TESSERACT_CUBE_COMBINED */)) {
@@ -16,20 +34,32 @@ vector<TextBox> findText(const Mat& img) {
         exit(1);
     }
 
-    Mat txt = Scalar::all(255) - img;
+    Mat scldown;
+    resize(pp, scldown, Size(0, 0), .25, .25, INTER_CUBIC);
+    imshow("text preprocessing", scldown);
+
     vector<TextBox> result;
 
     ocr.SetPageSegMode(tesseract::PSM_SPARSE_TEXT);
-    ocr.SetVariable("tessedit_char_whitelist", "0123456789px% .-");
-    ocr.SetImage(txt.data, txt.size().width, txt.size().height, txt.step[1], txt.step[0]);
-    Boxa* boxes = ocr.GetComponentImages(tesseract::RIL_TEXTLINE, true, NULL, NULL);
+    // ocr.SetVariable("tessedit_char_whitelist", "0123456789px% .-");
+    ocr.SetImage(pp.data, pp.size().width, pp.size().height, pp.step[1], pp.step[0]);
+    Boxa* boxes = ocr.GetComponentImages(tesseract::RIL_WORD, true, NULL, NULL);
     for (int i = 0; i < boxes->n; i++) {
         BOX* box = boxaGetBox(boxes, i, L_CLONE);
         ocr.SetRectangle(box->x, box->y, box->w, box->h);
         const char* ocrResult = ocr.GetUTF8Text();
-        if (*ocrResult != 0) {
+        if (ocrResult == nullptr) {
+            continue;
+        }
+        int conf = ocr.MeanTextConf();
+        if (*ocrResult != 0 && conf > 0) {
+            int x = round(box->x/UPSCALE);
+            int y = round(box->y/UPSCALE);
+            int w = round(box->w/UPSCALE);
+            int h = round(box->h/UPSCALE);
+            cerr << "text box @" << x << ',' << y << ',' << w << ',' << h << "; conf=" << conf << "; text='" << ocrResult << '\'' << endl;
             result.push_back(TextBox {
-                Rect(Point(box->x, box->y), Size(box->w, box->h)),
+                Rect(Point(x, y), Size(w, h)),
                 ocrResult });
         } else {
             delete[] ocrResult;
